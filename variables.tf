31eb22321fb93e5b220846bf0d82fc94ad164a80
ashwinbittu <ashwin.bittu@gmail.com>
2020-06-30 15:59:15 +1000

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "aws_region" {
+}
+
+variable "no_of_subnets" {
+}
+
+variable "aws_vpc_cidr_block" {
+}
+
+variable "app_env" {
+}
+
+variable "app_name" {
+}
+
+variable "app_csi" {
+}
+
+variable "app_color" {
+}
+
+variable "aws_vpc_instance_tenancy" {
+}
+
