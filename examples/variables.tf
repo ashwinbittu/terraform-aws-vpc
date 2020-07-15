5d7e627d18de169efed4159cf25f3e8408975939
ashwinbittu <ashwin.bittu@gmail.com>
2020-07-15 23:09:22 +1000

    Add files via upload

---
+++ examples/variables.tf
@@ -0,0 +1,21 @@
+variable "aws_region" {
+}
+
+variable "no_of_subnets" {
+}
+
+variable "aws_vpc_cidr_block" {
+}
+
+variable "app_env" {
+}
+
+variable "app_name" {
+}
+
+variable "app_csi" {
+}
+
+variable "aws_vpc_instance_tenancy" {
+}
+
